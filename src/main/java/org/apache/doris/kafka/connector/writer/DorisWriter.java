692e2de0c24ebc521e119a6938a9c0f098cf9c0f
wudi <676366545@qq.com>
2025-05-27 11:21:29 +0800

    update

--- src/main/java/org/apache/doris/kafka/connector/writer/DorisWriter.java
+++ src/main/java/org/apache/doris/kafka/connector/writer/DorisWriter.java
@@ -141,11 +141,13 @@ public abstract class DorisWriter {
             }
             processedOffset.set(dorisRecord.kafkaOffset());
         } else {
-            LOG.debug(
-                    "The record offset is smaller than processedOffset. recordOffset={}, offsetPersistedInDoris={}, processedOffset={}",
-                    record.kafkaOffset(),
-                    offsetPersistedInDoris.get(),
-                    processedOffset.get());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(
+                        "The record offset is smaller than processedOffset. recordOffset={}, offsetPersistedInDoris={}, processedOffset={}",
+                        record.kafkaOffset(),
+                        offsetPersistedInDoris.get(),
+                        processedOffset.get());
+            }
         }
     }
 
